d7f440913804d47a91c8bbce514a59e37144aa99
shubhampardeshi11 <shubham.pardeshi@shadesofweb.com>
2023-09-25 16:32:33 +0530

    NewsApp Using Class completed

---
+++ src/components/News.js
@@ -0,0 +1,112 @@
+import React, { Component } from 'react'
+import NewsItem from './NewsItem'
+import Spinner from './Spinner'
+import PropTypes from 'prop-types'
+import InfiniteScroll from "react-infinite-scroll-component";
+
+
+
+export class News extends Component {
+    static defaultProps = {
+        country: "in",
+        pageSize: 8,
+        category: "general"
+
+    }
+    static propTypes = {
+        country: PropTypes.string,
+        pageSize: PropTypes.number,
+        category: PropTypes.string
+    }
+
+    capitalizeFirstLetter = (string) => {
+        return `${string.charAt(0).toUpperCase()}${string.slice(1)}`;
+    }
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            articles: [],
+            loading: true,
+            page: 1,
+            totalResults: 0
+        }
+
+        document.title = `${this.capitalizeFirstLetter(this.props.category)} - NewsMonkey `
+    }
+
+
+
+    async UpdateNews() {
+        this.props.setProgress(0);
+        const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apiKey}&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+        this.setState({
+            loading: true
+        })
+        let data = await fetch(url);
+        this.props.setProgress(30);
+        let parsedData = await data.json();
+        this.props.setProgress(70);
+        console.log(parsedData);
+        this.setState({ articles: parsedData.articles, totalResults: parsedData.totalResults, loading: false });
+        this.props.setProgress(100);
+    }
+
+    async componentDidMount() {
+        this.UpdateNews();
+    }
+
+    fetchMoreData = async () => {
+        this.setState({ page: this.state.page + 1 })
+
+        const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apiKey}&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+        let data = await fetch(url);
+        let parsedData = await data.json();
+        console.log(parsedData);
+        this.setState({ articles: this.state.articles.concat(parsedData.articles), totalResults: parsedData.totalResults });
+    };
+
+    render() {
+        return (
+            <>
+                <div className='my-3'>
+                    <h1 className='my-3 text-center'>NewsMonkey - Top {this.capitalizeFirstLetter(this.props.category)} Headlines</h1>
+                    <InfiniteScroll
+                        dataLength={this.state.articles.length}
+                        next={this.fetchMoreData}
+                        hasMore={this.state.articles.length !== this.state.totalResults}
+                        loader={<Spinner />}
+                    >
+                        {this.state.loading && <Spinner />}
+
+                        <div className='container'>
+                            <div className="row my-3">
+                                {this.state.articles.map((element) => {
+                                    // Check if element.description and element.title are not null or undefined
+                                    const title = element.title ? element.title : "";
+                                    const description = element.description ? element.description : "";
+
+                                    return (
+                                        <div className="col-md-4" key={element.url}>
+                                            <NewsItem
+                                                title={title}
+                                                description={description}
+                                                imageUrl={element.urlToImage}
+                                                newsUrl={element.url}
+                                                author={element.author}
+                                                date={element.publishedAt}
+                                                source={element.source.name}
+                                            />
+                                        </div>
+                                    );
+                                })}
+                            </div>
+                        </div>
+                    </InfiniteScroll>
+                </div >
+            </>
+        )
+    }
+}
+
+export default News
---
+++ src/components/Spinner.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import Loading from './Loading.gif'
+
+export class Spinner extends Component {
+    render() {
+        return (
+            <div className='text-center'>
+                <img src={Loading} alt="Loading" />
+            </div>
+        )
+    }
+}
+
+
+export default Spinner
