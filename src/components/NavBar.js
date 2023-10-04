1fc994ffe4779e84e1c42f1fa81cb535218f8bb2
shubhampardeshi11 <shubham.pardeshi@shadesofweb.com>
2023-10-04 13:30:30 +0530

    Sticky Navbar & NewsMonkey Bugs Fixes

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,33 +1,31 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Link } from "react-router-dom";
 
 
-export class NavBar extends Component {
-    render() {
-        return (
-            <nav className="bg-dark navbar navbar-dark navbar-expand-lg">
-                <div className="container-fluid">
-                    <Link className="navbar-brand" to="/">NewsMonkey</Link>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="collapse navbar-collapse" id="navbarNav">
-                        <ul className="navbar-nav">
-                            
-                            <li className="nav-item"><Link className="nav-link" aria-current="page" to="/">Home</Link></li>
-                            <li className="nav-item"><Link className="nav-link" to="/business">Business</Link></li>
-                            <li className="nav-item"><Link className="nav-link" to="/entertainment">Entertainment</Link></li>
-                            <li className="nav-item"><Link className="nav-link" to="/general">General</Link></li>
-                            <li className="nav-item"><Link className="nav-link" to="/health">Health</Link></li>
-                            <li className="nav-item"><Link className="nav-link" to="/science">Science</Link></li>
-                            <li className="nav-item"><Link className="nav-link" to="/sports">Sports</Link></li>
-                            <li className="nav-item"><Link className="nav-link" to="/technology">Technology</Link></li>
-                        </ul>
-                    </div>
+const NavBar = () => {
+    return (
+        <nav className="bg-dark navbar navbar-dark fixed-top navbar-expand-lg">
+            <div className="container-fluid">
+                <Link className="navbar-brand" to="/">NewsMonkey</Link>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav">
+
+                        <li className="nav-item"><Link className="nav-link" aria-current="page" to="/">Home</Link></li>
+                        <li className="nav-item"><Link className="nav-link" to="/business">Business</Link></li>
+                        <li className="nav-item"><Link className="nav-link" to="/entertainment">Entertainment</Link></li>
+                        <li className="nav-item"><Link className="nav-link" to="/general">General</Link></li>
+                        <li className="nav-item"><Link className="nav-link" to="/health">Health</Link></li>
+                        <li className="nav-item"><Link className="nav-link" to="/science">Science</Link></li>
+                        <li className="nav-item"><Link className="nav-link" to="/sports">Sports</Link></li>
+                        <li className="nav-item"><Link className="nav-link" to="/technology">Technology</Link></li>
+                    </ul>
                 </div>
-            </nav>
-        )
-    }
+            </div>
+        </nav>
+    )
 }
 
 export default NavBar
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,26 +1,25 @@
 
-import React, { Component } from 'react'
+import React from 'react'
 
-export class NewsItem extends Component {
-    render() {
+const NewsItem = (props) => {
 
-        let { title, description, imageUrl, newsUrl, author, date, source } = this.props;
-        return (
+    let { title, description, imageUrl, newsUrl, author, date, source } = props;
+    return (
 
-            <div className="card my-3">
-                <div style={{ display: 'flex', justifyContent: 'flex-end', position: 'absolute', right: '0' }}>
-                    <span className="badge rounded-pill bg-danger"> {source} </span>
-                </div>
-                <img src={!imageUrl ? "https://bl-i.thgim.com/public/news/u2djw7/article67266141.ece/alternates/LANDSCAPE_1200/BL0409_Quantum_Second02.jpg" : imageUrl} className="card-img-top" alt="..." />
-                <div className="card-body">
-                    <h5 className="card-title">{title} </h5>
-                    <p className="card-text"> {description}</p>
-                    <p className="card-text"><small className="text-body-secondary">By {author ? author : "Unknown"} on {new Date(date).toGMTString()}</small></p>
-                    <a href={newsUrl} target="_blank" rel="noreferrer" className="btn btn-dark btn-sm">Read More</a>
-                </div>
+        <div className="card my-3">
+            <div style={{ display: 'flex', justifyContent: 'flex-end', position: 'absolute', right: '0' }}>
+                <span className="badge rounded-pill bg-danger"> {source} </span>
             </div>
-        )
-    }
+            <img src={!imageUrl ? "https://bl-i.thgim.com/public/news/u2djw7/article67266141.ece/alternates/LANDSCAPE_1200/BL0409_Quantum_Second02.jpg" : imageUrl} className="card-img-top" alt="..." />
+            <div className="card-body">
+                <h5 className="card-title">{title} </h5>
+                <p className="card-text"> {description}</p>
+                <p className="card-text"><small className="text-body-secondary">By {author ? author : "Unknown"} on {new Date(date).toGMTString()}</small></p>
+                <a href={newsUrl} target="_blank" rel="noreferrer" className="btn btn-dark btn-sm">Read More</a>
+            </div>
+        </div>
+    )
+
 }
 
 export default NewsItem
